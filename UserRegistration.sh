2c933a2d831b316600976ac0a9d3c19d82940dc9
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-30 17:17:52 +0530

    [BHUSHAN]Added:User First Name file.

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 	echo "Welcome to User Registration Form."
+       shopt -s extglob
+   echo "Enter User First Name:"
+   read name
+
+   namePat="^^[A-Z]{1}[a-z]{2,}$"
+
+        if [[ $name =~ $namePat ]]
+        then
+                echo CORRECT;
+        else
+                echo FAIL;
+        fi
+
+
